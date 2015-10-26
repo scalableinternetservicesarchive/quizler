10938ce94f682439ade8bd1ffd2b29f9298aba42
simejo <simen.johnsrud92@gmail.com>
2015-10-26 12:23:34 -0700

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,14 @@ User.create(username: 'Heather', email: 'heather@whatever.com', password: 'aweso
 User.create(username: 'Jeff', email: 'jeff@whatever.com', password: 'awesomepassword')
 User.create(username: 'Laura', email: 'laura@whatever.com', password: 'awesomepassword')
 User.create(username: 'Steve', email: 'steve@whatever.com', password: 'awesomepassword')
+
+Quiz.create(title: 'Soccer Quiz', description: 'A quiz about soccer', author: 'Heather')
+Quiz.create(title: 'Beer Quiz', description: 'A quiz about beer', author: 'Jeff')
+
+Question.create(question: 'Where is David Beckham from?', answer1: 'Spain', answer2: 'England', answer3: 'Norway', answer4: 'France', quiz_id: 1, correct_answer: 2)
+Question.create(question: 'In which league are Arsenal playing?', answer1: 'Premier League', answer2: 'La Liga', quiz_id: 1, correct_answer: 1)
+Question.create(question: 'Who won the World Cup in 2002?', answer1: 'France', answer2: 'Germany', answer3: 'Brazil' quiz_id: 1, correct_answer: 3)
+Question.create(question: 'What do the players kick?', answer1: 'Girls', answer2: 'Boys', answer3: 'Balls', answer4: 'Nuts' quiz_id: 1, correct_answer: 3)
+
+Question.create(question: 'Where can you buy a beer?', answer1: 'At the laundry', answer2: 'At a bar', answer3: 'In the kindergarden', quiz_id: 2, correct_answer: 2)
+Question.create(question: 'Which is NOT a beer?', answer1: 'Duff', answer2: 'Corona', answer3: 'Quizler', answer4: 'Heineken', quiz_id: 2, correct_answer: 3)
