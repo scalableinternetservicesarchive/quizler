a2c13ef9af172f51a3ef6a04c87359a82f6f1838
Francois Malinowski <francois@francois-malinowski.fr>
2015-11-05 01:30:35 -0800

    fm - Load tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  skip_before_action :verify_authenticity_token, if: :json_request?
-
-  protected
-
-  def json_request?
-    request.format.json?
-  end
-
 end
