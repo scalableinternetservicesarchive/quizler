13e10624e5d1b5abadb1988a563d7ab763894f47
Francois Malinowski <francois@francois-malinowski.fr>
2015-11-10 21:21:24 -0800

    fm - fix failing migration

--- db/migrate/20151111020520_add_authors_in_quizz_as_refrence_to_users.rb
+++ db/migrate/20151111020520_add_authors_in_quizz_as_refrence_to_users.rb
@@ -2,6 +2,6 @@ class AddAuthorsInQuizzAsRefrenceToUsers < ActiveRecord::Migration
   def change
     rename_column :quizzes, :author, :author_id
     add_index :quizzes, :author_id
-    add_foreign_key :quizzes, :author_id
+    add_foreign_key :quizzes, :users, column: 'author_id'
   end
 end
